b93ba38358dfd07aea8ccb46173ce7726a0e5d7b
omer ninio <niniomer@gmail.com>
2025-06-03 12:06:37 +0300

    Resolved merge conflict

--- js/storage.js
+++ js/storage.js
@@ -1,5 +1,8 @@
 // פונקציות כלליות לעבודה עם localStorage
 
+
+
+// כפתור התנתקות, הפנייה ללוגין במידה ולא מחובר
    function signOut() {
     localStorage.removeItem("currentUser");
     window.localStorage.href = "login.html";
@@ -20,3 +23,51 @@
         document.getElementById("userName").textContent = `Hi, ${userName}`;
     }
    }
+   
+
+// הצגת דירות עם כפתור FILTER
+  function displayListings(listings) {
+    const container = document.getElementById("listings-container");
+    container.innerHTML = "";
+
+    if (listings.length === 0) {
+        container.innerHTML = "<p>No listings found that matth the criteria!</p>";
+    } else {
+        listings.forEach(listing => {
+            const card = document.createElement("div");
+            card.className = "listing-card";
+            card.innerHTML = /**note the comments!!!!!!!! || נועד להצגת מידע לדירה שנמצאה בחיפוש**/
+            `
+                <h3>${listing.name}</h3>
+                <p>${listing.d}</p>
+                <img src="${/***listing picture url add later***/}" alt="${listing.name}"/> 
+                <p>Price: ${listing.price}</p>
+                <p>Score: ${listing.score}</p>
+                <p>Rooms: ${listing.rooms}</p>
+                <a href="${/***listing url add later***/}" class="btn">View Listings</a>
+                <button class="btn" onclick="addToFavorites(${listing.listing_id})">Add to favorites</button>
+            `;
+
+            container.appendChild(card);
+        });
+    }
+   }
+
+   // ערכים לסינון דירות
+   document.querySelector(".btn").addEventListener("click", function() {
+        const minScore = document.getElementById("scoreChoice").value;
+        const minPrice = document.getElementById("minPrice").value;
+        const maxPrice = document.getElementById("maxPrice").value;
+        const numOfRooms = document.getElementById("roomNumber").value;
+
+        const listingsFiltered = amsterdam.filter(listing => {
+            return (
+                (minScore && listing.score >= minScore) &&
+                (minPrice && listing.price >= minPrice) &&
+                (maxPrice && listing.price <= maxPrice) &&
+                (numOfRooms && listing.rooms === parseInt(numOfRooms))
+            );
+        });
+
+        displayListings(listingsFiltered);
+   });
